01bb80a0afcd963ceab937035de627b891a2a628
xerofenix <xerofe@tuta.io>
2026-02-15 23:57:16 +0530

    some chages donot know if work

--- src/db_config.zig
+++ src/db_config.zig
@@ -3,23 +3,23 @@ const pg = @import("pg");
 const zap = @import("zap");
 const dotenv = @import("dotenv");
 
-pub fn db_connect(allocator: *std.mem.Allocator) !*pg.Pool {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator_local = gpa.allocator();
-    defer _ = gpa.deinit();
+pub fn db_connect(allocator: std.mem.Allocator) !*pg.Pool {
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    // const allocator_local = gpa.allocator();
+    // defer _ = gpa.deinit();
 
     // Load environment variables from .env file
-    var env = try dotenv.init(allocator_local, ".env");
+    var env = try dotenv.init(allocator, ".env");
     defer env.deinit();
 
     const username = env.get("USERNAME") orelse "postgres";
-    const password = env.get("PASSWORD") orelse "postgres";
+    const password = env.get("PASSWORD") orelse "";
     const database = env.get("DATABASE") orelse "zap_crud";
     const host = env.get("HOST") orelse "localhost";
     const port = env.get("PORT") orelse "5432";
     const port_num = try std.fmt.parseInt(u16, port, 10);
 
-    const pool = try pg.Pool.init(allocator.*, .{ .size = 5, .connect = .{
+    const pool = try pg.Pool.init(allocator, .{ .size = 5, .connect = .{
         .port = port_num,
         .host = host,
     }, .auth = .{
@@ -29,7 +29,7 @@ pub fn db_connect(allocator: *std.mem.Allocator) !*pg.Pool {
         .timeout = 10_000,
     } });
 
-    std.debug.print("Connected to database: {s} at {s}:{d}\n", .{ database, host, port });
+    std.debug.print("Connected to database: {s} at {s}:{d}\n", .{ database, host, port_num });
 
     return pool; // Return the pointer to the allocated pool
 }
